8e7b586dcab545aa2861597f7a2448dd2440da68
dennislee <git@dennis.ninja>
2023-12-17 20:27:28 +0100

    Day 4

--- Day 4/Part2.py
+++ Day 4/Part2.py
@@ -2,11 +2,16 @@
 
 if __name__ == '__main__':
     data = open('input.txt').readlines()
-    total = 0
+    cards = []
 
-    for line in data:
+    for i in range(len(data)):
+        cards.append(1)
+
+    for i in range(len(data)):
+        line = data[i]
         winning_nos = {}
-        hits = -1
+        hits = 0
+
         for num in line[9:40].split():
             winning_nos [num] = ''
 
@@ -14,7 +19,13 @@ if __name__ == '__main__':
             if num in winning_nos:
                 hits += 1
 
-        if hits > -1:
-            total += pow (2, hits)
+        for j in range (hits):
+            cards[i+j+1] = cards[i+j+1] + cards[i]
 
+    total = 0
+    for i in cards:
+        total += i
+
+    print (cards)
     print (total)
+
